f55010c86a0ef71bb88d468a977b9dedf978c8c1
Matt Rothenberg <mattrothenberg@gmail.com>
2022-12-04 14:29:11 +0100

    feat: add fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,33 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import * as Fathom from "fathom-client";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === "development") return;
+
+    Fathom.load("XDKFLKSS", {
+      includedDomains: [
+        "https://badger-wtf.vercel.app",
+        "badge-wtf.vercel.app",
+      ],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
