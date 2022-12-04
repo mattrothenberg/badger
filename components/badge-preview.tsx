c33339a0314f377b814201edf5c296feae481509
Matt Rothenberg <mattrothenberg@gmail.com>
2022-12-04 19:44:37 +0100

    fix: add alt text to badge preview

--- components/badge-preview.tsx
+++ components/badge-preview.tsx
@@ -7,9 +7,17 @@ export function BadgePreview({
   url: string;
   params: ApiParamsV1;
 }) {
+  let labels = [params.startLabel, params.endLabel];
+  if (params.rtl) {
+    labels = labels.reverse();
+  }
+
   return (
     <div className="relative">
-      <img src={`/` + url} />
+      <img
+        alt={`Preview of badge that reads ${labels.join(" ")}`}
+        src={`/` + url}
+      />
     </div>
   );
 }
